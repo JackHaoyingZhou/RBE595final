cd17855185682572e5e5ead1a3471cec5fd5be95
JackHaoyingZhou <zhouhaoying96@outlook.com>
2020-10-01 19:47:33 -0400

    1

---
+++ test0.m
@@ -0,0 +1,9 @@
+tic
+%%%% measure time by tic and toc
+prompt = 'input matrix:\n';
+s = input(prompt);
+toc
+disp('the value of s is:');
+disp(s)
+% str = inputdlg('Enter a list of numbers separated by spaces or commas');
+% numbers = str2num(str{1});
