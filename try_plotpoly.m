c23f4587f8aba00fefb107ece0943ec3f15fe189
JackHaoyingZhou <zhouhaoying96@outlook.com>
2020-10-07 14:56:47 -0400

    2

--- try_plotpoly.m
+++ try_plotpoly.m
@@ -2,5 +2,10 @@ prompt = 'Please input the number of vertices: \n';
 num = input(prompt);
 %num = 6;
 [x,y,dt] = simple_polygon(num);
+vertices = [x';y'];
+%polygon_plot(vertices,'b')
+
+figure(1)
 plot(x,y)
+
 hold on;
